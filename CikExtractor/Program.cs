e67ece2c3c2a90b8854474a0c47afab0e7ac6040
LukeFZ <17146677+LukeFZ@users.noreply.github.com>
2023-08-18 22:02:43 +0200

    General overhaul + improvements
    - Added workflow for automatic releases
    - Added stub registry files to skip dumping of all registry hives
    - Convert to command-based CLI app, add multiple new commands

--- CikExtractor/Program.cs
+++ CikExtractor/Program.cs
@@ -1,67 +1,314 @@
-﻿using CikExtractor.Models;
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Text.Json;
+using Spectre.Console;
+using Spectre.Console.Cli;
 
 namespace CikExtractor;
 
 internal class Program
 {
-    private const string CikCachePath = "./Cik";
-    private const string DeviceKeyCache = "deviceKey.txt";
-
     static void Main(string[] args)
     {
-        Console.WriteLine("Parsing registry entries...");
-        var appLicenses = RegistryInterface.ParseRegistry();
-        Console.WriteLine("Registry loaded.");
+        var app = new CommandApp<DumpCommand>();
 
-        byte[]? deviceKey;
-
-        if (File.Exists(DeviceKeyCache))
+        app.Configure(ctx =>
         {
-            deviceKey = Convert.FromHexString(File.ReadAllText(DeviceKeyCache));
+            ctx.TrimTrailingPeriods(false);
+
+            ctx.AddCommand<DumpCommand>("dump")
+                .WithDescription("Derives the device key and decrypts all CIKs stored in the registry. Default command.");
+
+            ctx.AddCommand<ExportHiveCommand>("export-hive")
+                .WithDescription("Export the registry hive containing the CIKs into a file.");
+
+            ctx.AddCommand<ExportParametersCommand>("export-params")
+                .WithDescription(
+                    "Export the parameters needed to derive a device key.\nUseful if you want to run the key derivation on another device.");
+
+            ctx.AddCommand<DeriveKeyCommand>("derive")
+                .WithDescription("Derives a device key from the exported parameters of the 'export-params' command.");
+        });
+
+        app.Run(args);
+    }
+}
+
+internal sealed class DumpCommand : Command<DumpCommand.Settings>
+{
+    public sealed class Settings : CommandSettings
+    {
+        [DefaultValue("Cik")]
+        [Description("Folder to extract CIKs into.")]
+        [CommandOption("-c|--export-cik-path")]
+        public string? CikExtractionFolder { get; init; }
+
+        [DefaultValue("deviceKey.txt")]
+        [Description("File to read/write device key from/into.")]
+        [CommandOption("-d|--device-key-path")]
+        public string? DeviceKeyFile { get; init; }
+
+        [Description("Path to an already exported registry hive exported by the 'export-hive' command.")]
+        [CommandOption("-r|--reg-hive")]
+        public string? RegistryHiveFile { get; init; }
+    }
+
+    public override int Execute(CommandContext context, Settings settings)
+    {
+        Debug.Assert(settings.CikExtractionFolder != null, "settings.CikExtractionFolder != null");
+        Debug.Assert(settings.DeviceKeyFile != null, "settings.DeviceKeyFile != null");
+
+        byte[] deviceKey;
+
+        var manager = new RegistryManager();
+
+        string? registryPath;
+
+        if (settings.RegistryHiveFile != null)
+        {
+            registryPath = Path.GetFullPath(settings.RegistryHiveFile);
+
+            ConsoleLogger.WriteInfoLine($"Loading entries from [white bold]{registryPath}[/]...");
         }
         else
         {
-            if (appLicenses.All(license => license.LicenseType != LicenseType.Device))
+            registryPath = null;
+
+            ConsoleLogger.WriteInfoLine("Loading entries from registry...");
+        }
+
+        if (manager.LoadLicenses(registryPath))
+        {
+            ConsoleLogger.WriteInfoLine("Registry [green bold]loaded[/].");
+        }
+        else
+        {
+            ConsoleLogger.WriteErrLine("Failed to load registry.");
+            return -1;
+        }
+
+        var deviceKeyPath = Path.GetFullPath(settings.DeviceKeyFile);
+
+        if (File.Exists(deviceKeyPath))
+        {
+            ConsoleLogger.WriteInfoLine($"Reading device key from [white]{deviceKeyPath}[/].");
+            var deviceKeyFile = File.ReadAllText(deviceKeyPath);
+
+            Debug.Assert(deviceKeyFile.Length == 32, "deviceKeyFile.Length == 32");
+            Debug.Assert(deviceKeyFile.All("0123456789ABCDEFabcdef".Contains), "Device key is not a valid hex string");
+
+            deviceKey = Convert.FromHexString(deviceKeyFile);
+            ConsoleLogger.WriteInfoLine("[green bold]Successfully[/] loaded device key from file.");
+        }
+        else
+        {
+            ConsoleLogger.WriteInfoLine("Deriving device key...");
+            var parameters = DeviceKeyParameters.DumpParameters(manager);
+            if (parameters == null)
             {
-                Console.WriteLine("Error: Could not find device license in dumped registry.");
-                return;
+                ConsoleLogger.WriteErrLine("Failed to dump device key parameters.");
+                return -1;
             }
 
-            Console.WriteLine("Deriving device key...");
-            deviceKey = DeviceKeyDumper.DumpDeviceKey(appLicenses.First(license => license.LicenseType == LicenseType.Device).EncryptedDeviceKey!);
+            var key = DeviceKeyDumper.DeriveDeviceKey(parameters);
+            if (key == null)
+            {
+                ConsoleLogger.WriteErrLine("Failed to derive device key.");
+                return -1;
+            }
+
+            deviceKey = key;
+
+            var hexDeviceKey = Convert.ToHexString(deviceKey);
+
+            ConsoleLogger.WriteInfoLine($"[green bold]Successfully[/] derived device key [white bold]{hexDeviceKey}[/].");
+
+            File.WriteAllText(deviceKeyPath, hexDeviceKey);
         }
 
-        if (deviceKey == null)
+        var cikFolderPath = Path.GetFullPath(settings.CikExtractionFolder);
+        Directory.CreateDirectory(cikFolderPath);
+
+        var packageTable = new Table()
+            .Title("Packages")
+            .AddColumns(
+                new TableColumn("Package Name"),
+                new TableColumn("License Type"),
+                new TableColumn("Keys")
+            )
+            .RoundedBorder()
+            .Expand();
+
+        foreach (var license in manager.Licenses.Where(x => x.PackedContentKeys.Count > 0))
         {
-            Console.WriteLine("Error: Could not derive device key.");
-            return;
-        }
-
-        var deviceKeyHex = Convert.ToHexString(deviceKey);
-
-        Console.WriteLine($"Device Key loaded! Device Key: {deviceKeyHex}");
-        File.WriteAllText(DeviceKeyCache, deviceKeyHex);
-
-        Directory.CreateDirectory(CikCachePath);
-
-        foreach (var license in appLicenses.Where(lic => lic.PackedContentKeys.Count > 0))
-        {
-            Console.WriteLine($"Package Name: {license.PackageName} | License Type: {license.LicenseType} | Key Count: {license.PackedContentKeys.Count}");
+            var keysTable = new Table()
+                .AddColumns(
+                    new TableColumn("Key Id"),
+                    new TableColumn("Key")
+                )
+                .RoundedBorder()
+                .Expand();
 
             foreach (var pair in license.PackedContentKeys)
             {
                 var contentKey = Crypto.DecryptContentKey(deviceKey, pair.Value);
 
-                Console.WriteLine($"Key Id {pair.Key} | Key: {Convert.ToHexString(contentKey)}");
+                var filePath = Path.Join(cikFolderPath, $"{pair.Key}.cik");
 
-                var filePath = Path.Join(CikCachePath, $"{pair.Key}.cik");
-                var cikData = new byte[16 + contentKey.Length];
+                File.WriteAllBytes(filePath, 
+                    pair.Key.ToByteArray()
+                        .Concat(contentKey)
+                        .ToArray());
 
-                Buffer.BlockCopy(pair.Key.ToByteArray(), 0, cikData, 0, 16);
-                Buffer.BlockCopy(contentKey, 0, cikData, 16, contentKey.Length);
-
-                File.WriteAllBytes(filePath, cikData);
+                keysTable.AddRow(pair.Key.ToString(), Convert.ToHexString(contentKey));
             }
+
+            packageTable.AddRow(new Text(license.PackageName), new Text(license.LicenseType.ToString()), keysTable);
         }
+
+        AnsiConsole.Write(packageTable);
+
+        return 0;
+    }
+
+    public override ValidationResult Validate(CommandContext context, Settings settings)
+    {
+        if (File.Exists(settings.CikExtractionFolder))
+            return ValidationResult.Error("CIK extraction path must be a folder.");
+
+        if (File.Exists(settings.DeviceKeyFile))
+        {
+            var deviceKey = File.ReadAllText(settings.DeviceKeyFile);
+            if (deviceKey.Length != 32 || !deviceKey.All("0123456789ABCDEFabcdef".Contains))
+                return ValidationResult.Error("Provided device key in file is invalid. Must be 32 hex characters long.");
+        }
+
+        if (settings.RegistryHiveFile != null && !File.Exists(settings.RegistryHiveFile))
+            return ValidationResult.Error("Supplied registry hive does not exist.");
+
+        return ValidationResult.Success();
+    }
+}
+
+internal sealed class ExportHiveCommand : Command<ExportHiveCommand.Settings>
+{
+    public sealed class Settings : CommandSettings
+    {
+        [Description("Path to export the registry hive into.")]
+        [CommandArgument(0, "<path>")]
+        public string? ExportPath { get; init; }
+    }
+
+    public override int Execute(CommandContext context, Settings settings)
+    {
+        Debug.Assert(settings.ExportPath != null, "settings.ExportPath != null");
+
+        var manager = new RegistryManager();
+
+        var fullPath = Path.GetFullPath(settings.ExportPath);
+
+        manager.ExportHive(fullPath);
+
+        ConsoleLogger.WriteInfoLine($"[green bold]Successfully[/] exported registry hive into [white]{fullPath}[/]");
+
+        return 0;
+    }
+}
+
+internal sealed class ExportParametersCommand : Command<ExportParametersCommand.Settings>
+{
+    public sealed class Settings : CommandSettings
+    {
+        [Description("Path to export the parameters into.")]
+        [CommandArgument(0, "<path>")]
+        public string? ExportPath { get; init; }
+    }
+
+    public override int Execute(CommandContext context, Settings settings)
+    {
+        Debug.Assert(settings.ExportPath != null, "settings.ExportPath != null");
+
+        var manager = new RegistryManager();
+        ConsoleLogger.WriteInfoLine("Parsing registry entries...");
+        if (manager.LoadLicenses())
+        {
+            ConsoleLogger.WriteInfoLine("Registry [green bold]loaded[/].");
+        }
+        else
+        {
+            ConsoleLogger.WriteErrLine("Failed to load registry.");
+            return -1;
+        }
+
+        var parameters = DeviceKeyParameters.DumpParameters(manager);
+
+        var fullPath = Path.GetFullPath(settings.ExportPath);
+        var directory = Path.GetDirectoryName(fullPath);
+        if (directory != null)
+            Directory.CreateDirectory(directory);
+
+        File.WriteAllText(fullPath, JsonSerializer.Serialize(parameters));
+
+        ConsoleLogger.WriteInfoLine($"[green bold]Successfully[/] exported parameters into [white]{fullPath}[/]");
+
+        return 0;
+    }
+}
+
+internal sealed class DeriveKeyCommand : Command<DeriveKeyCommand.Settings>
+{
+    public sealed class Settings : CommandSettings
+    {
+        [Description("Path to the exported parameters.")]
+        [CommandArgument(0, "<path>")]
+        public string? ParametersPath { get; init; }
+
+        [DefaultValue("deviceKey.txt")]
+        [Description("Path to write the derived device key into.")]
+        [CommandArgument(1, "[output path]")]
+        public string? DeviceKeyPath { get; init; }
+    }
+
+    public override int Execute(CommandContext context, Settings settings)
+    {
+        Debug.Assert(settings.ParametersPath != null, "settings.ParametersPath != null");
+        Debug.Assert(settings.DeviceKeyPath != null, "settings.DeviceKeyPath != null");
+
+        var parameters = JsonSerializer.Deserialize<DeviceKeyParameters>(File.ReadAllText(settings.ParametersPath));
+        if (parameters == null)
+        {
+            ConsoleLogger.WriteErrLine("Failed to deserialize key parameters.");
+            return -1;
+        }
+
+        ConsoleLogger.WriteInfoLine("Imported parameters.");
+        ConsoleLogger.WriteInfoLine("Deriving device key...");
+
+        var key = DeviceKeyDumper.DeriveDeviceKey(parameters);
+        if (key == null)
+        {
+            ConsoleLogger.WriteErrLine("Failed to derive device key.");
+            return -1;
+        }
+
+        var hexDeviceKey = Convert.ToHexString(key);
+
+        ConsoleLogger.WriteInfoLine($"[green bold]Successfully[/] derived device key [white bold]{hexDeviceKey}[/].");
+
+        var fullKeyPath = Path.GetFullPath(settings.DeviceKeyPath);
+
+        File.WriteAllText(fullKeyPath, hexDeviceKey);
+
+        ConsoleLogger.WriteInfoLine($"[green bold]Successfully[/] wrote device key into [white bold]{fullKeyPath}[/].");
+
+        return 0;
+    }
+
+    public override ValidationResult Validate(CommandContext context, Settings settings)
+    {
+        if (settings.ParametersPath != null && !File.Exists(settings.ParametersPath))
+            return ValidationResult.Error("Parameters file does not exist.");
+
+        return ValidationResult.Success();
     }
 }
--- CikExtractor/RegistryInterface.cs
+++ CikExtractor/RegistryInterface.cs
@@ -4,26 +4,60 @@ using Registry;
 
 namespace CikExtractor;
 
-internal static class RegistryInterface
+internal sealed class RegistryManager
 {
     private const string RegExe = "reg";
-    private const string ControlRegKey = "HKLM\\SYSTEM\\ControlSet001\\Control\\";
-    private const string ExportRegFilename = "control_export.reghive";
-    private const string RegistryExportCommand = $"save {ControlRegKey} \"{{0}}/{ExportRegFilename}\" /Y";
+    private const string ControlRegKey = @"HKLM\SYSTEM\ControlSet001\Control\";
 
     private const string ExportCommand = "save {0} \"{1}\" /Y";
 
     // Registry Constants
-    private const string RootClipStoragePath = @"{7746D80F-97E0-4E26-9543-26B41FC22F79}";
+    private const string RootClipStoragePath = "{7746D80F-97E0-4E26-9543-26B41FC22F79}";
+    private const string LicenseBlobStorage = $"{RootClipStoragePath}\\{{A25AE4F2-1B96-4CED-8007-AA30E9B1A218}}";
+
+    // Unused here, just for reference
     private const string LicenseContentKeyIdStorage = $"{RootClipStoragePath}\\{{D73E01AC-F5A0-4D80-928B-33C1920C38BA}}";
     private const string LicencePolicyKeyStorage = $"{RootClipStoragePath}\\{{59AEE675-B203-4D61-9A1F-04518A20F359}}";
     private const string LicenseEntryIdStorage = $"{RootClipStoragePath}\\{{FB9F5B62-B48B-45F5-8586-E514958C92E2}}";
     private const string OptionalInfoStorage = $"{RootClipStoragePath}\\{{221601AB-48C7-4970-B0EC-96E66F578407}}";
-    private const string LicenseBlobStorage = $"{RootClipStoragePath}\\{{A25AE4F2-1B96-4CED-8007-AA30E9B1A218}}";
 
-    private static RegistryHive? _loadedRegistry;
+    private RegistryHive? _loadedRegistry;
 
-    public static void ExportRegistryHive(string hiveName, string outputPath)
+    public IEnumerable<SpLicense> Licenses
+    {
+        get
+        {
+            Debug.Assert(_loadedRegistry != null, "_loadedRegistry != null");
+            return _licenses ??= ParseLicenses();
+        }
+    }
+
+    private List<SpLicense>? _licenses;
+
+    public bool LoadLicenses(string? hivePath = null)
+    {
+        if (hivePath == null)
+        {
+            hivePath = Path.GetTempFileName();
+
+            ExportRegistryHive(ControlRegKey, hivePath);
+        }
+
+        return LoadHive(hivePath);
+    }
+
+    public void ExportHive(string path)
+    {
+        ExportRegistryHive(ControlRegKey, path);
+    }
+
+    private bool LoadHive(string hivePath)
+    {
+        _loadedRegistry = new RegistryHive(hivePath);
+        return _loadedRegistry.ParseHive();
+    }
+
+    private static void ExportRegistryHive(string hiveName, string outputPath)
     {
         using var process = new Process();
         process.StartInfo.Verb = "runas";
@@ -36,20 +70,11 @@ internal static class RegistryInterface
         process.WaitForExit();
     }
 
-    public static List<SpLicense> ParseRegistry()
+    private List<SpLicense> ParseLicenses()
     {
-        if (!File.Exists(ExportRegFilename))
-            ExportRegistryHive(ControlRegKey, Path.Join(Directory.GetCurrentDirectory(), ExportRegFilename));
+        Debug.Assert(_loadedRegistry != null, "_loadedRegistry != null");
 
-        _loadedRegistry = new RegistryHive(ExportRegFilename) {RecoverDeleted = false};
-        _loadedRegistry.ParseHive();
-
-        return ParseLicenses();
-    }
-
-    private static List<SpLicense> ParseLicenses()
-    {
-        var licenseBlobValues = _loadedRegistry!.GetKey(LicenseBlobStorage).Values;
+        var licenseBlobValues = _loadedRegistry.GetKey(LicenseBlobStorage).Values;
         return licenseBlobValues
             .Where(value => value.ValueDataRaw.Length > 8)
             .Select(licenseBlobValue => licenseBlobValue.ValueDataRaw)
